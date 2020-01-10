e9aef9dad79f04ce1373210b63c744c76c77e6e8
Jaime <Jaime@jaimes-mbp-356.lan>
2020-01-10 10:15:19 -0800

    Add #== method and corresponding tests

--- lib/project.rb
+++ lib/project.rb
@@ -8,4 +8,8 @@ class Project
         @id = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;").first.fetch('id').to_i
         self #self return to make #save safely chainable method (e.g. my_var = Project.new(params).save)
     end
+    def ==(comparator)
+        @title == comparator.title &&
+        @id == comparator.id
+    end
 end
