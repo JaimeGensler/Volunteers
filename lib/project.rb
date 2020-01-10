06f8b0d18c47079cb932183c587c088106a315d2
Jaime <Jaime@jaimes-mbp-356.lan>
2020-01-10 10:12:07 -0800

    Add #save method and corresponding tests

--- lib/project.rb
+++ lib/project.rb
@@ -4,4 +4,8 @@ class Project
         @title = attributes[:title]
         @id = attributes[:id]
     end
+    def save
+        @id = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;").first.fetch('id').to_i
+        self #self return to make #save safely chainable method (e.g. my_var = Project.new(params).save)
+    end
 end
